73ff0a617623aa1d507a8645b78ea79d25f3b69e
FariHafizh <142732331+FariHafizh@users.noreply.github.com>
2026-05-15 22:16:54 +0700

    buat jwt

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,17 @@
 from flask import Flask, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from db_setup import db, User 
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 
 # KONFIGURASI DATABASE
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cc_admin:cc123@localhost:5432/capstone_cc'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Kunci rahasia ini digunakan untuk "stempel" tiket agar tidak bisa dipalsukan
+app.config['JWT_SECRET_KEY'] = 'nutricheck-super-rahasia-2026' 
+jwt = JWTManager(app) # Inisialisasi mesin JWT
 
 db.init_app(app)
 
@@ -19,38 +24,64 @@ with app.app_context():
 def register():
     data = request.get_json()
     username = data.get('username')
+    email = data.get('email')
     password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'message': 'Username dan password diperlukan'}), 400
+    if not username or not email or not password:
+        return jsonify({'message': 'Username, email, dan password diperlukan'}), 400
 
-    if User.query.filter_by(username=username).first():
-        return jsonify({'message': 'Username sudah digunakan'}), 400
+    # Hanya cek email, karena username sekarang boleh sama
+    if User.query.filter_by(email=email).first():
+        return jsonify({'message': 'Email sudah digunakan'}), 400
 
     hashed_password = generate_password_hash(password)
-    new_user = User(username=username, password=hashed_password)
+    new_user = User(username=username, email=email, password=hashed_password)
     
     db.session.add(new_user)
-    db.session.commit() # Simpan ke PostgreSQL
+    db.session.commit()
 
     return jsonify({'message': 'User berhasil didaftarkan'}), 201
+
 
 @app.route('/login', methods=['POST'])
 def login():    
     data = request.get_json()
-    username = data.get('username')
+    # User login pakai email, bukan username
+    email = data.get('email') 
     password = data.get('password')
 
-    if not username or not password:
-        return jsonify({'message': 'Username dan password diperlukan'}), 400
+    if not email or not password:
+        return jsonify({'message': 'Email dan password diperlukan'}), 400
 
-    user = User.query.filter_by(username=username).first()
+    # Sistem mencari berdasarkan email
+    user = User.query.filter_by(email=email).first()
     
-    # Werkzeug mengecek apakah password yang diinput COCOK dengan hash di DB
     if user and check_password_hash(user.password, password):
-        return jsonify({'message': 'Login berhasil'}), 200
+        
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify({
+            'message': 'Login berhasil',
+            'username': user.username, # Kita bisa kembalikan nama usernya untuk ditampilkan di web
+            'access_token': access_token
+        }), 200
     else:
-        return jsonify({'message': 'Username atau password salah'}), 401     
+        return jsonify({'message': 'Email atau password salah'}), 401     
+    
+#Endpoint Percobaan JWT
+@app.route('/dashboard-test', methods=['GET'])
+@jwt_required() # Harus ada token untuk masuk.
+def dashboard_test():
+    # Mengambil ID user dari token yang sedang dipakai
+    current_user_id = get_jwt_identity()
+    
+    # Mencari siapa nama user pemilik ID tersebut di database
+    user = User.query.get(current_user_id)
+    
+    return jsonify({
+        'message': f'Akses ditolak untuk umum. Tapi selamat datang, {user.username}!',
+        'user_id': current_user_id,
+        'info': 'Ini adalah data rahasia dari halaman dashboard.'
+    }), 200
         
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/db_setup.py
+++ backend/db_setup.py
@@ -5,5 +5,6 @@ db = SQLAlchemy()
 # Mendefinisikan Tabel User untuk Login/Register
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
